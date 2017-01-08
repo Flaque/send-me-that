4831b293d84c9c305b7eadf105580f457e1673ed
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-01-08 11:33:59 -0700

    Fix codemirror's ref's testing in snapshot issue

--- src/editor/components/app/__test__/App.spec.js
+++ src/editor/components/app/__test__/App.spec.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import App from '../App.jsx';
 import renderer from 'react-test-renderer';
+import ReactTestUtils from 'react-addons-test-utils';
 
 test('Test that App is still the same', () => {
-  const tree = renderer.create(
-    <App />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+
+  // We're currently using the shallow renderer to avoid
+  // issues with refs in the CodeEditor module.
+  // See: https://github.com/facebook/react/issues/7148
+  const shallowRenderer = ReactTestUtils.createRenderer();
+  const result = shallowRenderer.render(<App />);
+  expect(result).toMatchSnapshot();
 })
--- src/editor/components/codeEditor/__test__/CodeEditor.spec.js
+++ src/editor/components/codeEditor/__test__/CodeEditor.spec.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import CodeEditor from '../CodeEditor.jsx';
 import renderer from 'react-test-renderer';
+import ReactTestUtils from 'react-addons-test-utils';
+
 
 test('Test that CodeEditor is still the same', () => {
-  const tree = renderer.create(
-    <CodeEditor />
-  ).toJSON();
-  expect(tree).toMatchSnapshot();
+
+  // We're currently using the shallow renderer to avoid
+  // issues with refs in the CodeEditor module.
+  // See: https://github.com/facebook/react/issues/7148
+  const shallowRenderer = ReactTestUtils.createRenderer();
+  const result = shallowRenderer.render(<CodeEditor />);
+  expect(result).toMatchSnapshot();
 })
